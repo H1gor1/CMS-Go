90e8595c2ebb218a1f22115440895ea989b54117
higor <higorgabrieldev@gmail.com>
2025-07-28 20:54:35 -0300

    feat: add routes logic

--- main.go
+++ main.go
@@ -1,16 +1,8 @@
 package main
 
-//por padrão se eu não definir um nome, ele considera o nome do pacote (gin) depois da ultima barra
-import "github.com/gin-gonic/gin"
+import "github.com/H1gor1/gopportunities/router"
 
 func main() {
-	//Inicializa o router com as configurações padrão do gin
-	router := gin.Default()
-	//Define uma rota GET para o caminho "/ping"
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+	//Initialize router
+	router.Initialize()
 }
--- router/router.go
+++ router/router.go
@@ -1,16 +1,13 @@
 package router
 
-//por padrão se eu não definir um nome, ele considera o nome do pacote (gin) depois da ultima barra
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	//Inicializa o router com as configurações padrão do gin
+	//Initialize the router with default settings
 	router := gin.Default()
-	//Define uma rota GET para o caminho "/ping"
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	//Initialize routes
+	initializeRoutes(router)
+
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
